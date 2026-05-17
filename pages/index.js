2a23fb6cc0d78c1508d5d1a178cae2f124485142
Franco Baumann <francobaumann@mac.home>
2026-05-17 13:00:59 +0200

    Fix button logic

--- pages/index.js
+++ pages/index.js
@@ -276,24 +276,7 @@ return (
     cursor: loading ? "not-allowed" : "pointer",
   }}
   disabled={loading}
-onClick={async () => {
-  if (credits <= 0) {
-    const res = await fetch("/api/create-checkout-session", {
-      method: "POST",
-    });
-
-    const data = await res.json();
-
-    if (data.url) {
-      window.location.href = data.url;
-    }
-
-    return;
-  }
-
-  generateReel();
-}}
->
+onClick={generateReel}>
 {mounted && (
   loading ? (
     "Generating..."
